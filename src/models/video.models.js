e44169065409cfd3e7a2b422faa2d704d3fb8360
Vineet Jaiswal <vineetjaiswal321@gmail.com>
2026-08-14 15:55:24 +0530

    ADD USER AND VIIDEO MODELS

---
+++ src/models/video.models.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile : {
+            type : String,
+            required : true
+        },
+        thumbnail : {
+            type : String,
+            required : true
+        },
+        title : {
+            type : String, 
+            required : true
+        },
+        discription : {
+            type : String, 
+            required : true
+        },
+        duration : {
+            type : Number, //cloudinary url
+            required : true
+        },
+        views : {
+            type : Number,
+            deafult : 0
+        },
+        isPublished : {
+            type : Boolean,
+            default : true
+        },
+        owner : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        }
+    },
+    {
+        timestamps : true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video=mongoose.model("Video", videoSchema)
