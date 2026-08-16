f2cc672f688a77a5e282f4adb117642df6ba48e9
Vineet <vineetjaiswal321@gmail.com>
2026-08-16 18:28:33 +0530

    Add refresh token functionality to user authentication

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -166,8 +166,56 @@ const logoutUser=asyncHandler(async (req, res)=>{
     .json(new ApiResponse(200, {}, "User logged out"))
 })
 
+
+const refreshAccessToken=asyncHandler(async (req, res)=>{
+    try {
+        const incommingRefreshToken=req.cookies.refreshToken || req.body.refreshToken
+
+        if(!incommingRefreshToken){
+            throw new ApiError(401, "Unauthorized Request")
+        }
+
+        const decodedToken=jwt.verify(incommingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+
+        const user=await User.findById(decodedToken?._id)
+
+        if(!user){
+            throw new ApiError(401, "Invalid refresh token")
+        }
+
+        if(incommingRefreshToken!==user?.refreshToken){
+            throw new ApiError(401, "Refresh token is expired or invalid")
+        }
+
+        const options={
+            httpOnly : true,
+            secure: true
+        }
+
+        const {accessToken, NewRefreshToken}=await generateAccessAndRefreshToken(user._id)
+
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken)
+        .cookie("refreshToken", NewRefreshToken)
+        .json(
+            new ApiResponse(
+                200,
+                {accessToken, refreshToken : NewRefreshToken},
+                "Access Token refreshed"
+            )
+        )
+
+
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refersh tokken")
+    }
+})
+
+
 export {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 }
